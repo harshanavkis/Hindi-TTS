d238f9075412e9eca71887960368a7207f9228aa
harshanavkis <harshanavkis@gmail.com>
2018-01-13 22:23:32 +0530

    Added Highway Nets model

---
+++ HighwayNet.py
@@ -0,0 +1,29 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.nn.functional as F
+from activations import getActivation as gA
+
+class HighwayFcNet(nn.Module):
+	"""
+		A more robust fully connected network
+		return: H*T + (1-T)x
+	"""
+	def __init__(self, input_size, numLayers, activation_type='ReLU',gate_activation='Sigmoid',bias=-1.0): #activation_type is a string containing the name of the activation
+		"""
+			We create a group of highway fc layers
+			All layers have the same number of units
+			Different number of units can be achieved through Plain Fully connected layers
+		"""
+		super(HighwayFcNet,self).__init__()
+		self.activation = gA(activation_type) #H func
+		self.gate_activation = gA(gate_activation)#T func
+		self.plain = nn.Linear(input_size,input_size)
+		nn.init.xavier_uniform(self.plain.weight)
+		self.gate = nn.Linear(input_size,input_size)
+		self.gate.bias.data.fill_(bias)
+
+	def forward(self,x):
+		h_out = self.activation(self.plain(x))
+		t_out = self.gate_activation(self.gate(x))
+		return torch.add(torch.mul(h_out,t_out),torch.mul((1.0-t_out),x))
---
+++ models.py
@@ -0,0 +1,18 @@
+import HighwayNet
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class HighwayFcModel(nn.Module):
+	def __init__(self, inDims, input_size, output_size, numLayers, activation='ReLU', gate_activation='Sigmoid', bias = -1.0):
+		super(HighwayFcModel,self).__init__()
+		self.highways = nn.ModuleList([HighwayNet.HighwayFcNet(input_size,numLayers,activation,gate_activation) for _ in range(numLayers)])
+		self.linear = nn.Linear(input_size,output_size)
+		self.dimChange  = nn.Linear(inDims, input_size)
+
+	def forward(self,x):
+		x = F.relu(self.dimChange(x))
+		for h in self.highways:
+			x = h(x)
+		x = F.softmax(self.linear(x))
+		return x 
